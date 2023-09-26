6c41e40693a5200b4405ed9793527923f4001b1d
Aektawan <s6506022620133@email.kmutnb.ac.th>
2023-09-26 10:14:18 +0700

    save

--- Server.js
+++ Server.js
@@ -1,21 +1,16 @@
-
-
-
 const express = require('express');
 const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
 const path = require("path");
+const { prototype } = require('events');
 
-//const base_url = 'http://localhost:3000';
-const base_url = 'http://node50138-nodetest.proen.app.ruk-com.cloud:11535';
+const base_url = 'https://node50138-nodetest.proen.app.ruk-com.cloud/';
 
 app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
-
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', async (req, res) => {
@@ -85,7 +80,7 @@ app.get("/delete/:id", async (req, res) => {
     }
 });
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080');
+port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
-
